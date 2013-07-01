9d888d58aa02098b4708a938f04cba1691e547b5
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2013-07-01 15:45:30 -0400

    1st gh-branch commit :)

--- config.rb
+++ config.rb
@@ -1,22 +1,21 @@
+require 'zurb-foundation'
 # Require any additional compass plugins here.
+
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "adventure-time-animated-lockscreen.theme/css"
+css_dir = "css"
 sass_dir = "sass"
-images_dir = "adventure-time-animated-lockscreen.theme/images"
-javascripts_dir = "adventure-time-animated-lockscreen.theme/js"
+images_dir = "img"
+javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
-# output_style = :compressed
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
-relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
 line_comments = false
 
 
